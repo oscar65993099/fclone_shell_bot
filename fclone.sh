94967290f64115b1f4a0c558e0e4b93ff4a9f60e
oscar65993099 <68966356+oscar65993099@users.noreply.github.com>
2020-08-01 08:23:27 +0800

    Update fclone.sh

--- fclone.sh
+++ fclone.sh
@@ -75,7 +75,7 @@ echo -e "▣▣▣▣▣▣▣正在执行转存▣▣▣▣▣▣▣\n"
 fclone copy goog:{$link} goog:{$myid}/"$rootName" --drive-server-side-across-configs --stats=1s --stats-one-line -vP --checkers=256 --transfers=320 --drive-pacer-min-sleep=1ms --check-first --min-size 10M --log-file=/root/gclone_log/"$rootName"'_copy1.txt'
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  拷贝完毕"
 echo -e "▣▣▣▣▣▣▣正在执行同步▣▣▣▣▣▣▣\n"
-fclone sync goog:{$link} goog:{$myid}/"$rootName" --drive-server-side-across-configs --stats=1s --stats-one-line -vP --checkers=256 --transfers=320 --drive-pacer-min-sleep=1ms --check-first --min-size 10M --drive-use-trash=false --log-file=/root/gclone_log/"$rootName"'_copy1.txt'
+fclone sync goog:{$link} goog:{$myid}/"$rootName" --drive-server-side-across-configs  --drive-use-trash=false --stats=1s --stats-one-line -vP --checkers=256 --transfers=320 --drive-pacer-min-sleep=1ms --check-first --min-size 10M --drive-use-trash=false --log-file=/root/gclone_log/"$rootName"'_copy1.txt'
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  同步完毕"
 echo -e "▣▣▣▣▣▣▣正在执行查重▣▣▣▣▣▣▣\n"
 fclone dedupe newest goog:{$myid}/"$rootName" --fast-list --drive-use-trash=false --no-traverse --size-only -v --log-file=/root/gclone_log/"$rootName"'_dedupe.txt'
